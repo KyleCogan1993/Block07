71ae6d1e08de613941c2bcc46d3a1e83b181851e
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-01-17 15:14:24 -0500

    updated the js to show the combination lock, readme has the pseudocode

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Block07
-Block07
+
+# Create greeting string
+
+# Create 1st number with additon
+
+# Create 2nd Number with muliplication
+
+# Create 3rd Number with subtraction
+
+# Create alert of greeting+1+2+3
---
+++ index.js
@@ -0,0 +1,15 @@
+// Created the four consts of the greeting string, and numbers for each of the combination numbers
+
+const greet = "You have received this message because you have been chosen to open an important value.  Here is the secret combination: ";
+
+//set the first to 10 using the + operator
+const first = 8+2;
+
+//set the second to 40 using the * operator
+const second = 2*20;
+
+//set the third to 39 using the - operator
+const third = 40-1;
+
+//creates the popup alert of the greeting with the three constants concat'd
+alert(greet + first + "-" + second + "-" + third);
